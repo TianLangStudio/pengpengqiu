c54eef26f9e394ee72c1750ccf76a5a3f9d2b1d6
Tianlang <tianlangstuido@aliyun.com>
2019-09-11 12:31:39 +0800

    add yarn.lock and upgrade version of socket.io

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var app = require('http').createServer(handler)
+var app = require('express')()
+    ,http = require('http').createServer(app)
 	,fs = require('fs')
-	,io = require('socket.io').listen(app)
+	,io = require('socket.io')(http)
 	,Game = require('./Game').Game;
 	
-app.listen(8888);
+http.listen(8888);
 console.log("listen on port 8888");
 function handler(request, response) {
 
@@ -13,55 +14,67 @@ function handler(request, response) {
 		response.end();
 	});
 }
-io.set('log level', 0);
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+//io.set('log level', 0); 2.2 not supply
 var games = {},//保存房间的game对象
 	game = new Game({});
 	
-io.sockets.on('connection', function( socket ) {
+io.on('connection', function( socket ) {
     socket.emit('gameInfo', game.info());//初始化游戏状态
 	socket.on( 'join' , function(data){
-	    
-		if( io.sockets.clients(data.roomId)   &&io.sockets.clients(data.roomId).length === 2 ){
-			socket.emit( 'roomful', data);
-			socket.leave();
-		} else {
-			socket.join(data.roomId);
-			
-			if( io.sockets.clients(data.roomId) && io.sockets.clients(data.roomId).length === 2 ){
-			    io.sockets.in(data.roomId).emit('twoplayer', data);
-				/*socket.in(data.roomId).broadcast.emit('twoplayer', data);
-				socket.in(data.roomId).emit('twoplayer', data);*/
-				//玩家到齐 开始游戏
+	   
+        console.dir(data);
+        io.clients(function(err, clients) {
+          if(clients && clients.length > 2) {
+             socket.emit( 'roomful', data);     
+             socket.leave();
+              console.log("roomful");
+              console.dir(clients);
+          } else {
+             socket.join(data.roomId);
+		    	console.log("room");
+                console.log(socket.id);
+                console.dir(clients);
+			 io.clients(function(err, roomClients) {
+                if( roomClients.length === 2 ){
+			            io.in(data.roomId).emit('twoplayer', data);
+				        /*socket.in(data.roomId).broadcast.emit('twoplayer', data);
+				        socket.in(data.roomId).emit('twoplayer', data);*/
+				        //玩家到齐 开始游戏
+				        
+				        games[data.roomId] = new Game({
+					        onWin:function(winData) {
+						        io.in(data.roomId).emit('win',winData);
+					        },
+					        onFlush:function(gameInfo) {
+						        io.in(data.roomId).emit('gameInfo',gameInfo);
+					        }
+					        
+				        });
+				        games[data.roomId].begin();
+				        
 				
-				games[data.roomId] = new Game({
-					onWin:function(winData) {
-						io.sockets.in(data.roomId).emit('win',winData);
-					},
-					onFlush:function(gameInfo) {
-						io.sockets.in(data.roomId).emit('gameInfo',gameInfo);
-					}
-					
-				});
-				games[data.roomId].begin();
-				
-				
-			} else {
-			    data.userName = "A";
-			    io.sockets.in(data.roomId).emit('oneplayer', data);
-				/*
-				socket.in(data.roomId).broadcast.emit('oneplayer', data);
-				socket.in(data.roomId).emit('oneplayer', data);*/
-				
-			}
-			//玩家离开房间游戏结束
-			socket.on('disconnect', function() {
-			    var game = games[data.roomId];//停止游戏
-				if(game){
-					game.stop();
-				}
-				io.sockets.in(data.roomId).emit('leave', data);
-			})
-		}
+			        } else {
+			            data.userName = "A";
+			            io.in(data.roomId).emit('oneplayer', data);
+				        /*
+				        socket.in(data.roomId).broadcast.emit('oneplayer', data);
+				        socket.in(data.roomId).emit('oneplayer', data);*/
+				        
+			        }
+			        //玩家离开房间游戏结束
+			        socket.on('disconnect', function() {
+			            var game = games[data.roomId];//停止游戏
+				        if(game){
+					        game.stop();
+				        }
+				        io.in(data.roomId).emit('leave', data);
+			        })
+             });
+          }
+        });
 	});
 	socket.on('board2Left',function(data){//球拍向左移动
 		games[data.roomId].board2Left(data);
